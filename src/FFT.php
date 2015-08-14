cf46faeff6b9a5bbedde6f627ced8a5a32672841
Nick Barham <Nik@brokencube.co.uk>
2015-08-15 00:42:42 +0100

    Rewrote FFT to cache some reusable data for about ~20% speed up over 100 iterations

--- src/FFT.php
+++ src/FFT.php
@@ -13,44 +13,65 @@ class FFT
         }
         return $output;
     }
-    
-    public static function run(array $real, array $im = null)
+
+    public static function run(array $input_real, array $input_im = null)
     {
-        $length = count($real);
-        $halfLength = $length / 2;
-        if (!$im) $im = array_fill(0, $length, 0);
-        if ($length < 2) return [$real, $im];
-        
-        for($i = 0; $i < $halfLength; ++$i) {
-            $even_real[$i] = $real[$i*2];
-            $odd_real[$i] = $real[($i*2)+1];
-            $even_im[$i] = $im[$i*2];
-            $odd_im[$i] = $im[($i*2)+1];
-        }
-        
-        list($even_real, $even_im) = static::run($even_real, $even_im);
-        list($odd_real, $odd_im) = static::run($odd_real, $odd_im);
+    	static $fft, $tr, $ti, $tlength;
+		$length = count($input_real);
 		
-        for ($i = 0; $i < $halfLength; ++$i) {
-            $p = $i / $length;
-            $t = -2 * M_PI * $p;
-            
-            // $t = $t->cexp();
-            $t_real = cos($t);
-            $t_im = sin($t);
-            
-            // $t = $t->mul($odd[$i]);
-            $t_real2 = ($t_real * $odd_real[$i]) - ($t_im * $odd_im[$i]);
-            $t_im2 = ($t_real * $odd_im[$i]) + ($t_im * $odd_real[$i]);
-            
-            // $return[$i] = $even[$i]->add($t);
-            $return_real[$i] = $even_real[$i] + $t_real2;
-            $return_im[$i] = $even_im[$i] + $t_im2;
-            
-            //$return[$i + $halfLength] = $even[$i]->sub($t);
-            $return_real[$i + $halfLength] = $even_real[$i] - $t_real2;
-            $return_im[$i + $halfLength] = $even_im[$i] - $t_im2;
-        }
-        return [$return_real, $return_im];
-    }
+		// Use previously calculated values if length of data is the same as the last FFT we ran
+		if ($tlength != $length)
+		{
+			$tlength = $length;
+			$halfLength = $length / 2;
+			for ($i = 0; $i < $halfLength; ++$i) {
+				$p = $i / $length;
+				$t = -2 * M_PI * $p;
+				$tr[$i] = cos($t);
+				$ti[$i] = sin($t);
+			}
+			
+			$fft = function ($real, $im) use (&$fft, $tr, $ti, $tlength) {
+				$length = count($real);
+				$halfLength = $length / 2;
+				if (!$im) $im = array_fill(0, $length, 0);
+				if ($length < 2) return [$real, $im];
+	
+				
+				for($i = 0; $i < $halfLength; ++$i) {
+					$even_real[$i] = $real[$i*2];
+					$odd_real[$i] = $real[($i*2)+1];
+					$even_im[$i] = $im[$i*2];
+					$odd_im[$i] = $im[($i*2)+1];
+				}
+	
+				list($even_real, $even_im) = $fft($even_real, $even_im);
+				list($odd_real, $odd_im) = $fft($odd_real, $odd_im);
+	
+				for ($i = 0; $i < $halfLength; ++$i) {
+	
+					// $t = $t->cexp(); -- Precalculated for ~20% speed improvement
+					$p = $i / $length;
+					$t_real = $tr[$p * $tlength];
+					$t_im = $ti[$p * $tlength];
+	
+					// $t = $t->mul($odd[$i]);
+					$t_real2 = ($t_real * $odd_real[$i]) - ($t_im * $odd_im[$i]);
+					$t_im2 = ($t_real * $odd_im[$i]) + ($t_im * $odd_real[$i]);
+	
+					// $return[$i] = $even[$i]->add($t);
+					$return_real[$i] = $even_real[$i] + $t_real2;
+					$return_im[$i] = $even_im[$i] + $t_im2;
+	
+					//$return[$i + $halfLength] = $even[$i]->sub($t);
+					$return_real[$i + $halfLength] = $even_real[$i] - $t_real2;
+					$return_im[$i + $halfLength] = $even_im[$i] - $t_im2;
+				}
+				return [$return_real, $return_im];
+			};
+		}
+
+
+		return $fft($input_real, $input_im);
+	}
 }
